c25edb25f5fa2c857243ab6d3976b5a25adf26c9
Ritik Nandanwar <nandanwarritik10@gmail.com>
2021-03-22 23:08:48 +0530

    First commit

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int number;
+    int TWO = 2;
+    cout<< "Enter a number" << endl;
+    cin >> number;
+    for(int i =2 ; i < number ; i++){
+        if(number % i ==0) {
+            cout<<number;
+            cout<<" IS NOT A PRIME NUMBER "<<endl;
+            break;
+        }
+        else{
+            cout<<"IS A PRIME "<<endl;
+            break;
+        }
+    }
+    
+    return 0;
+}
